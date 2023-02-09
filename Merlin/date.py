151699efeb57358f3e91e4ff4b57d379738433e9
anatoly <aabelyakov@mail.ru>
2023-02-09 09:27:07 +0300

    q

--- Merlin/date.py
+++ Merlin/date.py
@@ -20,3 +20,28 @@ print(today.replace(day=1))
 
 last_month = today.replace(day=1) - timedelta(days=1)
 print(last_month.strftime("%Y-%m-%d"))
+
+# Найти последний день текущего месяца
+import calendar
+import datetime
+
+# Get the current month and year
+now = datetime.datetime.now()
+year = now.year
+month = now.month
+print(111, month)
+
+# Get the last day of the current month
+_, last_day = calendar.monthrange(year, month)
+
+print("The last day of the current month is {}".format(last_day))
+
+# Найти последний день текущего месяца (второй вариант)
+# Первое число следующего месяца
+next_month = today.replace(month=month+1,day=1)
+print("today", today)
+print("next_month", next_month)
+
+# Последний день текущего месяца
+cur_month = next_month - timedelta(days=1)
+print("cur_month", cur_month)
